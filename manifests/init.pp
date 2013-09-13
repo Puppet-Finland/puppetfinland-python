080a65cd707422c5b721de15fea743750d3aa906
Samuli Seppänen <samuli@openvpn.net>
2013-09-13 19:52:38 +0000

    Added default values to Hiera checks
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,7 @@
 class python {
 
 # Rationale for this is explained in init.pp of the sshd module
-if hiera('manage_python') != 'false' {
+if hiera('manage_python', 'true') != 'false' {
     include python::install
 }
-
 }
