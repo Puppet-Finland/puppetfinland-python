5a48d403571baea5bbd0a02ef848614400285565
Samuli Seppänen <samuli.seppanen@gmail.com>
2014-11-20 18:25:52 +0200

    Fixes to rdoc
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,20 @@
+#
+# == Class: python
+#
+# This is a dummy class. The actual work is done in the subclasses.
+#
+# == Authors
+#
+# Samuli Seppänen <samuli.seppanen@gmail.com>
+#
+# Samuli Seppänen <samuli@openvpn.net>
+#
+# Mikko Vilpponen <vilpponen@protecomp.fi>
+#
+# == License
+#
+# BSD-license. See file LICENSE for details.
+#
 class python {
 
 # Rationale for this is explained in init.pp of the sshd module
